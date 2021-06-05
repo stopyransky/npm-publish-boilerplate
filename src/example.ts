acfe1a9c98db210cc557f95053b56d997df64c3b
Karol Stopyra <stopyransky@gmail.com>
2021-06-05 11:54:25 +0100

    feat: feature test

--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,20 @@
+interface IParams {
+  name: string;
+  type: string;
+}
+class Example {
+  params: IParams;
+  printed: boolean;
+  constructor(parameters) {
+    this.params = parameters;
+
+    this.printed = helloWorld();
+  }
+}
 function helloWorld(): boolean {
   // eslint-disable-next-line no-console
   console.log('hello world');
   return true;
 }
 
-export { helloWorld };
+export { Example, helloWorld };
